a8c10776f7cc910d47d29886ab7ef2c8266965fa
Simon Yen <simonyen980821@gmail.com>
2024-06-26 08:45:12 +0800

    添加更多变量

--- fish/.config/fish/conf.d/proxy.fish
+++ fish/.config/fish/conf.d/proxy.fish
@@ -1,12 +1,22 @@
+# proxy [Address of proxy]
 function proxyon
-	set -gx HTTPS_PROXY http://localhost:7890
-	set -gx HTTP_PROXY http://localhost:7890
-	set -gx NO_PROXY localhost,127.0.0.1,.example.com
+    set -Ux all_proxy http://$argv[(count $argv)]:7890
+    set -Ux http_proxy http://$argv[(count $argv)]:7890
+    set -Ux https_proxy http://$argv[(count $argv)]:7890
+    set -Ux ALL_PROXY http://$argv[(count $argv)]:7890
+    set -Ux HTTP_PROXY http://$argv[(count $argv)]:7890
+    set -Ux HTTPS_PROXY http://$argv[(count $argv)]:7890
+    echo all_proxy=$all_proxy
+    echo http_proxy=$http_proxy
+    echo https_proxy=$https_proxy
 end
 
 function proxyoff
-	set -e HTTPS_PROXY
-	set -e HTTP_PROXY
-	set -e NO_PROXY
+    set -e all_proxy
+    set -e http_proxy
+    set -e https_proxy
+    set -e ALL_PROXY
+    set -e HTTP_PROXY
+    set -e HTTPS_PROXY
 end
-	
+
